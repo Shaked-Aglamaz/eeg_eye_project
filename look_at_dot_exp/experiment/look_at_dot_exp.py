6fcfef772f30f3288773b631ebfdb061299ccf1d
shaked <shaked.aglamaz@gmail.com>
2022-09-11 13:53:05 +0300

    look at dot exp final!

--- look_at_dot_exp/experiment/look_at_dot_exp.py
+++ look_at_dot_exp/experiment/look_at_dot_exp.py
@@ -1,13 +1,14 @@
 import random
+import pandas as pd
 import pylsl as lsl
 from psychopy import core, parallel, visual
 
-##Initializing game
-
-# # create a window
-
+# create a window
 win = visual.Window([800, 600], fullscr=True, monitor="testMonitor", units="deg", color=(-1, -1, -1), useFBO=False)
-options = [f'figures_decoding_exp/fig{i}.png' for i in range(240)]
+
+subject_id = '001'
+num_images = 240
+options = [f'figures_decoding_exp/fig{i}.png' for i in range(num_images)]
 info = lsl.StreamInfo(name='Trigger Stream', type='Markers', channel_count=1, channel_format='int32', source_id='uniqueid12345')
 outlet = lsl.StreamOutlet(info, chunk_size=0)
 
@@ -18,6 +19,7 @@ def main():
     win.update()
     core.wait(5)
     random.shuffle(options)
+    pd.DataFrame(options).to_csv(f'shuffle_order_{subject_id}.csv')
     for image in options:
         parallel.setData(0)
         slidePic1 = visual.ImageStim(win, image="fixation.jpg",  units='norm', size=[2,2], interpolate = True)
